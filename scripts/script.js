1732cdfb0484b6e5227c735b1d5a6a365449d610
flymo5678 <flynn@littlebricks.org>
2023-02-03 20:48:50 +0000

    death

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,23 @@
 var character = document.getElementById("character");
 document.addEventListener("click", jump);
 function jump() {
-    if (character.classList == "animate"){return;}
-    character.classList.add("animate");
+    if (character.classList == "jump"){return;}
+    character.classList.add("jump");
     setTimeout(removeJump, 500);
 }
 
 function removeJump() {
-    character.classList.remove("animate")
+    character.classList.remove("jump")
 }
+
+var block = document.getElementById("block");
+block.classList.add("blockmove")
+function checkDead() {
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if (blockLeft < 50 && blockLeft > -50 && characterTop >= 380) {
+        alert("Game Over!");
+    }
+}
+
+setInterval(checkDead, 10)
