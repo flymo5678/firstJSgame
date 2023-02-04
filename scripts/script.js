a50dff3d4ee80d5b62d17e5a71e24fdde177fec1
flymo5678 <flynn@littlebricks.org>
2023-02-04 09:50:32 +0000

    death works gooder

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,20 @@
+var startButton = document.getElementById("startButton");
+startButton.addEventListener("click", startGame);
+
+var death = document.getElementById("death")
+death.classList.add("hidden")
+
+function startGame() {
+    startButton.classList.add("hidden");
+    setTimeout(sprites, 1000)
+}
+
+function sprites() {
+    document.addEventListener("click", jump);
+    block.classList.add("blockmove");
+}
+
 var character = document.getElementById("character");
-document.addEventListener("click", jump);
 function jump() {
     if (character.classList == "jump"){return;}
     character.classList.add("jump");
@@ -10,13 +25,16 @@ function removeJump() {
     character.classList.remove("jump")
 }
 
+
+
 var block = document.getElementById("block");
-block.classList.add("blockmove")
 function checkDead() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     if (blockLeft < 50 && blockLeft > -50 && characterTop >= 380) {
-        alert("Game Over!");
+        document.removeEventListener("click", jump)
+        block.classList.remove("blockmove")
+        death.classList.remove("hidden")
     }
 }
 
