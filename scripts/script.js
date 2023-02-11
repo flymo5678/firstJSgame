bd216a5f02e06a01752c18097cc548330f524248
flymo5678 <86871317+flymo5678@users.noreply.github.com>
2023-02-11 18:18:12 +0000

    update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -3,12 +3,18 @@ var scoring
 var scoreEnabled = true
 var score = 0;
 var scoreCounter = document.getElementById("currentScore");
+var blockSpeed = 5000
+var jumpLength = 2500
+
+var root = document.documentElement;
 
 var startButton = document.getElementById("startButton");
 startButton.addEventListener("click", startGame);
 
 var death = document.getElementById("death");
 death.classList.add("hidden");
+
+var block = document.getElementById("block");
 
 function startGame() {
     startButton.classList.add("hidden");
@@ -22,12 +28,60 @@ function displayScore() {
         scoreEnabled = false
         score += 1;
         scoreCounter.innerText = text.concat(score);
+        changeSpeed()
     } else if (blockLeft > 0 && scoreEnabled == false) {
         scoreEnabled = true
     }
 }
 
+function changeSpeed() {
+    if (score == 1) {
+        blockSpeed = 4000
+        jumpLength = 2000
+        block.classList.remove("blockmove")
+        root.style.setProperty('--jumpLength', jumpLength + "ms")
+        if (blockLeft = 450) {
+            root.style.setProperty('--blockSpeed', blockSpeed + "ms")
+            setTimeout(function(){block.classList.add("blockmove")}, 10)
+        }
+    }
+    if (score == 2) {
+        blockSpeed = 2000
+        jumpLength = 1500
+        block.classList.remove("blockmove")
+        root.style.setProperty('--jumpLength', jumpLength + "ms")
+        if (blockLeft = 450) {
+            root.style.setProperty('--blockSpeed', blockSpeed + "ms")
+            setTimeout(function(){block.classList.add("blockmove")}, 10)
+        }
+    }
+    if (score == 3) {
+        blockSpeed = 1750
+        jumpLength = 1500
+        block.classList.remove("blockmove")
+        root.style.setProperty('--jumpLength', jumpLength + "ms")
+        if (blockLeft = 450) {
+            root.style.setProperty('--blockSpeed', blockSpeed + "ms")
+            setTimeout(function(){block.classList.add("blockmove")}, 10)
+        }
+    }
+    if (score == 5) {
+        blockSpeed = 1250
+        jumpLength = 750
+        block.classList.remove("blockmove")
+        character.classList.remove("jump")
+        root.style.setProperty('--jumpLength', jumpLength + "ms")
+        character.classList.add("jump")
+        if (blockLeft = 450) {
+            root.style.setProperty('--blockSpeed', blockSpeed + "ms")
+            setTimeout(function(){block.classList.add("blockmove")}, 10)
+        }
+    }
+}
+
 function sprites() {
+    root.style.setProperty('--blockSpeed', blockSpeed + "ms")
+    root.style.setProperty('--jumpLength', jumpLength + "ms")
     document.addEventListener("click", jump);
     block.classList.add("blockmove");
 }
@@ -36,14 +90,13 @@ var character = document.getElementById("character");
 function jump() {
     if (character.classList == "jump"){return;}
     character.classList.add("jump");
-    setTimeout(removeJump, 500);
+    setTimeout(removeJump, jumpLength);
 }
 
 function removeJump() {
     character.classList.remove("jump");
 }
 
-var block = document.getElementById("block");
 function checkDead() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
